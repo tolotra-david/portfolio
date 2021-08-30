ccb806f7b84732ec824b3f3894dabbe2e3326613
Tolotra David <tolotradavid2@gmail.com>
2021-08-30 16:06:32 +0300

    sidebar && home ok

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,7 +6,7 @@ import {
   ElementRef,
   Output,
   EventEmitter,
-  AfterViewInit
+  AfterViewInit,
 } from '@angular/core';
 
 @Component({
@@ -14,52 +14,51 @@ import {
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
-export class SidebarComponent implements OnInit, AfterViewChecked, AfterViewInit {
+export class SidebarComponent
+  implements OnInit, AfterViewChecked, AfterViewInit
+{
   name: string = 'rakotorina';
   firstName: string = 'tolotra david';
   statu: string = 'frontend developer';
   navigations: Array<string> = [];
-  
+
   @ViewChild('navigationMenu')
   private navigationMenu: ElementRef;
-  
+
   @ViewChild('backgroundActive')
   private backgroundActive: ElementRef;
 
-  @Output() isClicked = new EventEmitter<boolean>()
+  @Output() isClicked = new EventEmitter<boolean>();
 
   constructor() {}
 
   ngOnInit(): void {
-    this.navigations = ['accueil', 'competences', 'projets']
+    this.navigations = ['accueil', 'competences', 'projets'];
   }
 
   ngAfterViewChecked(): void {
-  }
-  
-  ngAfterViewInit(): void {
     this.loadActive();
   }
+
+  ngAfterViewInit(): void {}
 
   loadActive() {
     const ul: HTMLUListElement = this.navigationMenu.nativeElement;
     let menus: HTMLCollection = ul.children;
-    setTimeout(() => {
-      for (let i = 0; i < menus.length; i++) {
-        if (menus[i].className === 'active') {
-          this.givePosition(menus[i] as HTMLElement);
-        }
+    for (let i = 0; i < menus.length; i++) {
+      if (menus[i].className === 'active') {
+        this.givePosition(menus[i] as HTMLElement);
       }
-    }, 0)
+    }
   }
 
-  giveOutput(): void{
-    this.isClicked.emit(true)
+  giveOutput(): void {
+    this.isClicked.emit(true);
   }
 
   givePosition(link: HTMLElement) {
-    const backgroundElement = this.backgroundActive.nativeElement;
-    backgroundElement.style.height =  link.offsetHeight + 'px'
-    backgroundElement.style.top = link.offsetTop + 'px';
+    const backgroundElement: HTMLDivElement = this.backgroundActive.nativeElement;
+    backgroundElement.style.height = link.offsetHeight + "px"
+    backgroundElement.style.transform = "translateY(" + link.offsetTop + "px)";
   }
 }
