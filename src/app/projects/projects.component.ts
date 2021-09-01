558cf92ef8a8522bdd1ed63ce01bac3f43b51c2b
Tolotra David <tolotradavid2@gmail.com>
2021-09-01 15:49:25 +0300

    projects en place

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -3,33 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent implements OnInit {
-
   projects = [
     {
-      title: 'crud',
-      techno: 'angular 12, angular material',
-      img: '../../assets/images/crudmean.jpeg',
-      class: 'angular'
+      title: 'NSI-MADA',
+      description: "J'ai créer ce site en 2020.",
+      techno: 'WORDPRESS, ELEMENTOR',
+      img: '/assets/images/nsi-mada.png',
     },
     {
-      title: 'simple todolist',
-      techno: 'react v.16, material design',
-      img: '../../assets/images/react.svg',
-      class: 'react'
+      title: 'GESTIONNAIRE DE STOCK PHARMACIE',
+      description:
+        'Jai appris le Framwork Angular grâce à mon stage chez Young-dev.',
+      techno:
+        'ANGULAR, ANGULAR MATERIAL, NGX-BOOTSTRAP, NGX-DATATABLE, NGX-CHART',
+      img: '/assets/images/young-dev.png',
+      class: 'react',
     },
     {
-      title: 'crud',
-      techno: 'php v.7, mysql, bootstrap 4',
-      img: '../../assets/images/phpmysql.png',
-      class: 'phpmysql'
-    }
-  ]
-  constructor() { }
+      title: 'MODAL et CAROUSEL',
+      description:
+        "Je me suis mis à l'épreuve de créer un Modal et un Carousel avec Javascript sans utiler un Framwork.",
+      techno:
+        'HTML5, CSS3, JAVASCRIPT',
+      img: '../../assets/images/vanilla.png',
+    },
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/single-project/single-project.component.ts
+++ src/app/single-project/single-project.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-single-project',
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleProjectComponent implements OnInit {
 
+  @Input()
+  title: string
+  @Input()
+  description: string
+  @Input()
+  techno: string
+  @Input()
+  imgSrc: string
   constructor() { }
 
   ngOnInit(): void {
