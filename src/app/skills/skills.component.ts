ca806c1af6909e22ab17799cabe754374cb10461
Tolotra David <tolotradavid2@gmail.com>
2022-08-18 11:46:01 +0300

    delete last design skill component

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,23 +1,16 @@
-import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+// import {Chart}  from 'chart.js';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent implements OnInit {
-  skills = [
-    { langage: 'ALGORITHIME', width: 65 },
-    { langage: 'HTML & CSS', width: 90 },
-    { langage: 'JAVASCRIPT/TYPESCRIPT', width: 80 },
-    { langage: 'ANGULAR/ANGULAR MATERIAL', width: 80 },
-    { langage: 'Node.js/Express.js', width: 30 },
-    { langage: 'PHP version 5 et 7', width: 60 },
-    { langage: 'REACT', width: 25 },
-  ];
-  isLoaded = false;
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
 }
